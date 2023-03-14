9c2b4a0f28c02c4816fe9a74f17c1cb4f9d90b54
clever-junior <tk_clever.junior@outlook.com>
2023-03-14 14:50:23 -0400

    feat: add requisite 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+
+    if not target_time:
+        return None
+
+    for time in permanence_period:
+        index = 0
+
+        if not all(isinstance(hour, int) for hour in time):
+            return None
+
+        if target_time in range(time[index], time[index + 1] + 1):
+            counter += 1
+
+    return counter
