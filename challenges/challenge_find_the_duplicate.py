0b31560fb6f1d99717efef80e71c821e5ac50b44
clever-junior <tk_clever.junior@outlook.com>
2023-03-14 19:07:11 -0400

    feat: add requisite 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,12 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    index = 0
+
+    nums.sort()
+
+    for num in nums[:-1]:
+        index += 1
+        if not isinstance(num, int) or num < 0:
+            return False
+        if num == nums[index]:
+            return num
+    return False
