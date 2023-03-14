2676f39bf337bf039f15a22066f3bef2df0b1230
clever-junior <tk_clever.junior@outlook.com>
2023-03-14 16:05:12 -0400

    fix: lint issues

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,12 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if not word:
-       return False
+        return False
 
     if high_index > low_index:
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
 
     if word[low_index] != word[high_index]:
         return False
-    
+
     return True
-    
